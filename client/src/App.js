acd8f726b01b71cb5555716291d685541513e2b6
netta8 <netta7398@gmail.com>
2023-11-28 14:44:35 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+function App() {
+  return (
+    <main className="area">
+      <ul className="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <Navbar />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </ul>
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/data.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    title: "Bean Cafe",
+    subtitle: "HTML, CSS, Bootstrap",
+    description:
+      "In a collaborative effort with a skilled team, we developed a website for a local coffee shop using HTML, CSS, and Bootstrap. Our focus was on capturing the shop's ambiance and showcasing its products through a visually appealing and responsive design. The project highlighted our technical expertise and effective teamwork.",
+    image: "./Bean-cafe.png",
+    link: "#",
+  },
+  {
+    title: "Special needs center app",
+    subtitle: "React",
+    description:
+      "I've created a responsive website using React JS to empower parents of children with disabilities. This platform allows parents to easily enroll their children in personalized classes at the center, ranging from swimming and karate to dance. The site aims to enhance accessibility and streamline the enrollment process for families.",
+    image: "./NB-special-needs-center.png",
+    link: "#",
+  },
+  {
+    title: "Beyond Work",
+    subtitle: "MERN APP",
+    description:
+      "BeyondWord is MERN stack group project, developed by four junior developers as a part of the Final Project of the DCI Further Training as a Web & Software Developer. Beyond Work is a platform for employees to connect on a personal level.",
+    image: "./Beyond-work.png",
+    link: "https://beyondworkclient.onrender.com/",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "#",
+  },
+];
+
+export const skills = [
+  { title: "HTML", image: "./html-5--v1.png" },
+  { title: "CSS", image: "./css3.png" },
+  { title: "SASS", image: "./sass.png" },
+  { title: "Bootstrap", image: "./bootstrap.png" },
+  { title: "Material UI", image: "./logo.png" },
+  { title: "Tailwind CSS", image: "./tailwind-css-1-2.svg" },
+  { title: "React", image: "./react-native.png" },
+  { title: "JavaScript", image: "./javascript--v1.png" },
+  { title: "MongoDB", image: "./mongodb.png" },
+  { title: "NodeJS", image: "./nodejs.png" },
+  { title: "Git", image: "./git.png" },
+  { title: "NPM", image: "./npm.png" },
+  { title: "Postman-API", image: "./postman-api.png" },
+  { title: "Figma", image: "./5968704.png" },
+];
