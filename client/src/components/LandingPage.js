ef66f2d022eb5044dd788ecae737a8ac9dfbefda
netta8 <netta7398@gmail.com>
2023-12-06 16:30:00 +0100

    last changes for today

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -4,15 +4,16 @@ import "../App.css";
 export default function LandingPage() {
   return (
     <div>
-      <div className="flex flex-wrap ">
-        <h5 className="title-font text-green-400 text-xl mb-4 font-medium typewriter ">
+      <div className=" w-56 ">
+        <h5 className=" title-font text-green-400 text-xl mb-4 font-medium typewriter ">
           Hi, my name is
         </h5>
       </div>
+      <div className="flex flex-wrap "></div>
       <main>
         <div className="flex flex-col ">
-          <h1 className="title-font text-9xl mb-4 font-medium">Netta Barel</h1>
-          <h3 className=" text-4xl mb-4 ">A Full-Stack Web Developer</h3>
+          <h1 className="title-font text-7xl mb-4 font-medium">Netta Barel</h1>
+          <h3 className=" text-3xl mb-4 ">A Full-Stack Web Developer</h3>
         </div>
       </main>
     </div>
